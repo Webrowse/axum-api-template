c37201ae8ff9abdf098291b6b79e4800dd38096c
Webrowse <great.adarsh@gmail.com>
2025-11-30 01:15:01 +0530

    display JSON on health route using serde and axum

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, routing::get};
+use serde::Serialize;
 
 #[tokio::main]
 async fn main(){
-    let app = Router::new().route("/", get(root));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
 
@@ -11,4 +14,21 @@ async fn main(){
 
 async fn root() -> &'static str {
     "Welcome to this API"
+}
+
+#[derive(Serialize)]
+pub struct HealthData {
+    name: String,
+    age: u8,
+    over_sixty: bool,
+}
+
+async fn health() -> Json<HealthData> {
+    let hd = HealthData {
+        name: String::from("Adarsh"),
+        age: 12,
+        over_sixty: false,
+    };
+
+    Json(hd)
 }
