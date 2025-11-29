915d6ce8f8f1d232ecacc48467a8280dfd502528
Webrowse <great.adarsh@gmail.com>
2025-11-29 23:51:14 +0530

    axum server started with / path

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-fn main() {
-    println!("Hello, Adarsh!");
-    
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main(){
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Welcome to this API"
 }
